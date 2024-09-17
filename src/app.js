01e60971bcb35a0e2b554fe69e0b0ddb26f82c43
AnuragKush13 <anurag.kush13@gmail.com>
2024-09-17 22:06:12 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+app.use("/hello",(req,res)=>{
+    res.send("Hello Hello Hello")
+})
+app.use("/test",(req,res)=>{
+    res.send("Test hello!!")
+})
+app.use("/",(req,res)=>{
+    res.send("Hello World!!")
+});
+
+
+app.listen(7777,()=>{
+    console.log("Server is listening on port 7777")
+});
