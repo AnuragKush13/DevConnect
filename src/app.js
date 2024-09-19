9e1d4524b14a7e3f67a73b4717a49b57050a3049
AnuragKush13 <anurag.kush13@gmail.com>
2024-09-19 23:11:32 +0530

    middlewares-example-basic

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require('express')
 
 const app = express();
+
+app.use("/admin",(req,res,next)=>{
+    const token = '12345';
+    const userAuthorizer = token === '12345';
+    if(userAuthorizer)
+        next();
+    else
+    res.status(401).send("Unauthorized to access!!");
+})
+app.use("/admin/getData",(req,res)=>{
+    res.send("Fetched important data successfully!!")
+})
+app.use("/admin/deleteData",(req,res)=>{
+    res.send("Deleted useless data successfully!!")
+})
+/*
+//Middlewares
 app.use("/",(req,res,next)=>{
     console.log("from /");
     // res.send("from / send")
@@ -21,7 +38,7 @@ app.use("/user",(req,res,next)=>{
     console.log("inside third");
     res.send("from 3rd route");
 })
-
+*/
 
 app.listen(7777,()=>{
     console.log("Server is listening on port: 7777")
