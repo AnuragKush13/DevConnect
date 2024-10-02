29b7bb59d9aea22219fce2230153197ece63d84e
AnuragKush13 <anurag.kush13@gmail.com>
2024-10-02 19:46:53 +0530

    added feed api, blocked some users

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/authentication');
-const connectionRequestModel = require('../models/connectionRequest');
+const ConnectionRequestModel = require('../models/connectionRequest');
 const userRouter = express.Router();
+const User = require('../models/user')
 const USER_SAFE_DATA = "firstName lastName age about gender skills photoUrl";
 userRouter.get("/user/requests/received",userAuth,async (req,res)=>{
     try{
         const loggedInUser = req.user;
         //status == interested
         //touserId = loggedInUserId
-        const connectionRequest = await connectionRequestModel.find({
+        const connectionRequest = await ConnectionRequestModel.find({
             status :"interested",
             toUserId:loggedInUser._id
         }).populate("fromUserId",USER_SAFE_DATA)
@@ -23,7 +24,7 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
     try{
         const loggedInUser = req.user;
 
-        const connectionRequest = await connectionRequestModel.find({
+        const connectionRequest = await ConnectionRequestModel.find({
             $or:[
                 {toUserId:loggedInUser._id,
                     status:"accepted"
@@ -44,6 +45,34 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
     catch(err)
     {
         res.status(400).send("ERRRO:: "+err.message)
+    }
+})
+
+userRouter.get("/feed",userAuth,async (req,res)=>{
+    try{
+
+        const loggedInUser = req.user;
+        //remove loggedinuser
+        //remove those users whose ids are present in connections table
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or:[
+                {toUserId:loggedInUser._id },
+                {fromUserId:loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")
+       const hideUsersFromFeed = new Set();
+       connectionRequest.forEach((request)=>{
+        hideUsersFromFeed.add(request.fromUserId);
+        hideUsersFromFeed.add(request.toUserId);
+       })
+
+       const feedUsers = await User.find({
+        _id: {$nin: Array.from(hideUsersFromFeed)}
+       }).select(USER_SAFE_DATA)
+       res.send(feedUsers)
+    }
+    catch(err){
+        res.status(400).send("ERROR::"+err.message)
     }
 })
 
